3794f66a1c56d6d809792eb35d679a6481346591
Isaac Laughlin <isaac.laughlin@galvanize.com>
2017-04-27 17:37:41 -0700

    Initial commit.

---
+++ init.el
@@ -0,0 +1,69 @@
+;;No scrolling no tool-bar.
+(if window-system
+    (tool-bar-mode 0))
+(if window-system
+     (scroll-bar-mode -1))
+(setq mac-option-key-is-meta 1)
+
+;Show column.
+(setq column-number-mode t)
+
+;;Packages
+(require 'package)
+;;(require 'melpa)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+;;(setq exec-path (append exec-path '("/usr/local/bin")))
+
+;;
+(setq custom-file "~/.emacs.d/custom.el")
+
+(defvar myPackages
+  '(elpy
+    company
+    find-file-in-project
+    highlight-indentation
+    ivy
+    keywiz
+    magit
+    git-commit
+    magit-popup
+    markdown-mode
+    material-theme
+    pyenv-mode
+    python-mode
+    pythonic
+    f
+    pyvenv
+    s
+    with-editor
+    dash
+    async
+    yaml-mode
+    yasnippet
+    better-shell
+))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+(load-theme 'material t) ;; load material theme
+
+
+;;Elpy
+(elpy-enable)
+(elpy-use-ipython)
+(pyenv-mode)
+(setq python-shell-interpreter "ipython3" python-shell-interpreter-args "--simple-prompt --pprint")
+
+;;m magit
+(global-set-key (kbd "C-x g") 'magit-status)
